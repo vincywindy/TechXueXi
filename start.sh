324d1be4be4aba3ddd9cb8a5ce45986c09ea29b5
windy liu <vincywindy@live.cn>
2021-08-27 15:09:51 +0800

    修改输出重定向

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./run.sh >> /dev/console 2>&1 &
-echo "30 9 * * * /xuexi/run.sh >> /dev/console 2>&1\n#empty line" > /etc/cron.d/mycron
+./run.sh > /proc/1/fd/1 &
+echo "30 9 * * * /xuexi/run.sh > /proc/1/fd/1\n#empty line" > /etc/cron.d/mycron
 crontab /etc/cron.d/mycron
 cron -f
